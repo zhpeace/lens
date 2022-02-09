265654cadd4762a69f129a320fe26b44e667dd45
Janne Savolainen <janne.savolainen@live.fi>
2022-02-09 11:03:31 +0200

    Remove explicit singleton as lifecycle for being default
    Co-authored-by: Mikko Aspiala <mikko.aspiala@gmail.com>
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

--- src/renderer/components/cluster-manager/bottom-bar-items.injectable.ts
+++ src/renderer/components/cluster-manager/bottom-bar-items.injectable.ts
@@ -2,7 +2,7 @@
  * Copyright (c) OpenLens Authors. All rights reserved.
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
-import { getInjectable, lifecycleEnum } from "@ogre-tools/injectable";
+import { getInjectable } from "@ogre-tools/injectable";
 import { computed } from "mobx";
 import rendererExtensionsInjectable from "../../../extensions/renderer-extensions.injectable";
 import type { StatusBarRegistration } from "./status-bar-registration";
@@ -20,8 +20,6 @@ const bottomBarItemsInjectable = getInjectable({
         .sort(leftItemsBeforeRight),
     );
   },
-
-  lifecycle: lifecycleEnum.singleton,
 });
 
 export default bottomBarItemsInjectable;
