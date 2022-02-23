52f46b179674c46457f24004c879a7463820af04
Janne Savolainen <janne.savolainen@live.fi>
2022-02-23 16:05:37 +0200

    WIP Co-authored-by: Mikko Aspiala <mikko.aspiala@gmail.com>
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

--- src/renderer/frames/cluster-frame/cluster-frame.tsx
+++ src/renderer/frames/cluster-frame/cluster-frame.tsx
@@ -5,17 +5,14 @@
 import React from "react";
 import { makeObservable, computed, IComputedValue } from "mobx";
 import { disposeOnUnmount, observer } from "mobx-react";
-import { Redirect, Route } from "react-router";
+import { Redirect } from "react-router";
 import { ConfirmDialog } from "../../components/confirm-dialog";
 import { DeploymentScaleDialog } from "../../components/+workloads-deployments/deployment-scale-dialog";
 import { CronJobTriggerDialog } from "../../components/+workloads-cronjobs/cronjob-trigger-dialog";
-import { ClusterPageRegistry, getExtensionPageUrl } from "../../../extensions/registries/page-registry";
-import { ClusterPageMenuRegistration, ClusterPageMenuRegistry } from "../../../extensions/registries";
 import { StatefulSetScaleDialog } from "../../components/+workloads-statefulsets/statefulset-scale-dialog";
 import { ReplicaSetScaleDialog } from "../../components/+workloads-replicasets/replicaset-scale-dialog";
 import { CommandContainer } from "../../components/command-palette/command-container";
 import * as routes from "../../../common/routes";
-import { TabLayout, TabLayoutRoute } from "../../components/layout/tab-layout";
 import { ErrorBoundary } from "../../components/error-boundary";
 import { MainLayout } from "../../components/layout/main-layout";
 import { Notifications } from "../../components/notifications";
@@ -74,60 +71,60 @@ class NonInjectedClusterFrame extends React.Component<Dependencies> {
       : routes.workloadsURL();
   }
 
-  getTabLayoutRoutes(menuItem: ClusterPageMenuRegistration) {
-    const routes: TabLayoutRoute[] = [];
+  // getTabLayoutRoutes(menuItem: ClusterPageMenuRegistration) {
+  //   const routes: TabLayoutRoute[] = [];
+  //
+  //   if (!menuItem.id) {
+  //     return routes;
+  //   }
+  //
+  //   ClusterPageMenuRegistry.getInstance().getSubItems(menuItem).forEach((subMenu) => {
+  //     const page = ClusterPageRegistry.getInstance().getByPageTarget(subMenu.target);
+  //
+  //     if (page) {
+  //       routes.push({
+  //         routePath: page.url,
+  //         url: getExtensionPageUrl(subMenu.target),
+  //         title: subMenu.title,
+  //         component: page.components.Page,
+  //       });
+  //     }
+  //   });
+  //
+  //   return routes;
+  // }
 
-    if (!menuItem.id) {
-      return routes;
-    }
+  // renderExtensionTabLayoutRoutes() {
+  //   return ClusterPageMenuRegistry.getInstance().getRootItems().map((menu, index) => {
+  //     const tabRoutes = this.getTabLayoutRoutes(menu);
+  //
+  //     if (tabRoutes.length > 0) {
+  //       const pageComponent = () => <TabLayout tabs={tabRoutes}/>;
+  //
+  //       return <Route key={`extension-tab-layout-route-${index}`} component={pageComponent} path={tabRoutes.map((tab) => tab.routePath)}/>;
+  //     } else {
+  //       const page = ClusterPageRegistry.getInstance().getByPageTarget(menu.target);
+  //
+  //       if (page) {
+  //         return <Route key={`extension-tab-layout-route-${index}`} path={page.url} component={page.components.Page}/>;
+  //       }
+  //     }
+  //
+  //     return null;
+  //   });
+  // }
 
-    ClusterPageMenuRegistry.getInstance().getSubItems(menuItem).forEach((subMenu) => {
-      const page = ClusterPageRegistry.getInstance().getByPageTarget(subMenu.target);
-
-      if (page) {
-        routes.push({
-          routePath: page.url,
-          url: getExtensionPageUrl(subMenu.target),
-          title: subMenu.title,
-          component: page.components.Page,
-        });
-      }
-    });
-
-    return routes;
-  }
-
-  renderExtensionTabLayoutRoutes() {
-    return ClusterPageMenuRegistry.getInstance().getRootItems().map((menu, index) => {
-      const tabRoutes = this.getTabLayoutRoutes(menu);
-
-      if (tabRoutes.length > 0) {
-        const pageComponent = () => <TabLayout tabs={tabRoutes}/>;
-
-        return <Route key={`extension-tab-layout-route-${index}`} component={pageComponent} path={tabRoutes.map((tab) => tab.routePath)}/>;
-      } else {
-        const page = ClusterPageRegistry.getInstance().getByPageTarget(menu.target);
-
-        if (page) {
-          return <Route key={`extension-tab-layout-route-${index}`} path={page.url} component={page.components.Page}/>;
-        }
-      }
-
-      return null;
-    });
-  }
-
-  renderExtensionRoutes() {
-    return ClusterPageRegistry.getInstance().getItems().map((page, index) => {
-      const menu = ClusterPageMenuRegistry.getInstance().getByPage(page);
-
-      if (!menu) {
-        return <Route key={`extension-route-${index}`} path={page.url} component={page.components.Page}/>;
-      }
-
-      return null;
-    });
-  }
+  // renderExtensionRoutes() {
+  //   return ClusterPageRegistry.getInstance().getItems().map((page, index) => {
+  //     const menu = ClusterPageMenuRegistry.getInstance().getByPage(page);
+  //
+  //     if (!menu) {
+  //       return <Route key={`extension-route-${index}`} path={page.url} component={page.components.Page}/>;
+  //     }
+  //
+  //     return null;
+  //   });
+  // }
 
   render() {
     const currentRoute = this.props.currentRoute.get();
