f59f85ace0cc28286a9e60305019a12247bdf3da
Sebastian Malton <sebastian@malton.name>
2022-03-14 10:17:43 -0400

    fix build
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/main/catalog-sources/kubeconfig-sync-manager/kubeconfig-sync-manager.injectable.ts
+++ src/main/catalog-sources/kubeconfig-sync-manager/kubeconfig-sync-manager.injectable.ts
@@ -6,8 +6,6 @@ import { getInjectable } from "@ogre-tools/injectable";
 import directoryForKubeConfigsInjectable from "../../../common/app-paths/directory-for-kube-configs/directory-for-kube-configs.injectable";
 import { KubeconfigSyncManager } from "./kubeconfig-sync-manager";
 import { createClusterInjectionToken } from "../../../common/cluster/create-cluster-injection-token";
-import { ClusterManager } from "../../cluster-manager";
-import { ClusterStore } from "../../../common/cluster-store/cluster-store";
 
 const kubeconfigSyncManagerInjectable = getInjectable({
   id: "kubeconfig-sync-manager",
@@ -15,9 +13,6 @@ const kubeconfigSyncManagerInjectable = getInjectable({
   instantiate: (di) => new KubeconfigSyncManager({
     directoryForKubeConfigs: di.inject(directoryForKubeConfigsInjectable),
     createCluster: di.inject(createClusterInjectionToken),
-    // TODO: make injectable
-    deleting: ClusterManager.getInstance().deleting,
-    getClusterById: (id) => ClusterStore.getInstance().getById(id),
   }),
 });
 
